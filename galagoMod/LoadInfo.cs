4a2c7f2b66527be1fce45219f817ca1072e16422
reeeeemo <robbie@oxley.ca>
2024-02-06 12:14:00 -0500

    first

---
+++ galagoMod/LoadInfo.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx.Hacknet;
+using BepInEx;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace galagoMod
+{
+    [BepInPlugin(ModGUID, ModName, ModVer)]
+    public class GalagoPlugin : HacknetPlugin
+    {
+        public const string ModGUID = "com.Windows10CE.Template";
+        public const string ModName = "Galago";
+        public const string ModVer = "1.0.0";
+
+        public override bool Load()
+        {
+            return true;
+        }
+    }
+}
