b9a57cf39f2f6ce9bc0a41857138b0175a8e0fd2
reeeeemo <robbie@oxley.ca>
2024-04-15 17:07:54 -0400

    [FIX] GUID + ModVer Txt Change

--- galagoMod/LoadInfo.cs
+++ galagoMod/LoadInfo.cs
@@ -10,9 +10,9 @@ namespace galagoMod
     [BepInPlugin(ModGUID, ModName, ModVer)]
     public class GalagoPlugin : HacknetPlugin
     {
-        public const string ModGUID = "com.Windows10CE.Template";
+        public const string ModGUID = "com.reeeeemo.galago";
         public const string ModName = "Galago";
-        public const string ModVer = "1.0.0";
+        public const string ModVer = "1.0.1";
 
         public override bool Load()
         {
